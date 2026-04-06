0fba41be86e95374b9783b27abb6568533cacb6f
bugrabalim11 <bugrablm11@gmail.com>
2026-04-06 14:17:07 +0300

    OOP temelleri: Class ve Property tanımlamaları yapıldı. Kalıtım mimarisi uygulandı. UI tarafında tasarımsal iyileştirmeler eklendi.

--- Proje_OOP/Controllers/DefaultController.cs
+++ Proje_OOP/Controllers/DefaultController.cs
@@ -93,5 +93,45 @@ namespace Proje_OOP.Controllers
             ViewBag.d = Cumle();
             return View();
         }
+
+        public IActionResult Deneme()
+        {
+            Sehirler sehirler = new Sehirler(); //Nesne türetildi
+
+            sehirler.SehirAd = "Ankara";
+            sehirler.SehirId = 1;
+            sehirler.Nufus = 1000000;
+            sehirler.Ulke = "Türkiye";
+            sehirler.Renk1 = "Kırmızı";
+            sehirler.Renk2 = "Beyaz";
+
+            ViewBag.v1 = sehirler.SehirId;
+            ViewBag.v2 = sehirler.Ulke;
+            ViewBag.v3 = sehirler.SehirAd;
+            ViewBag.v4 = sehirler.Nufus;
+            ViewBag.v5 = sehirler.Renk1;
+            ViewBag.v6 = sehirler.Renk2;
+
+            sehirler.SehirAd = "Bakü";
+            sehirler.SehirId = 2;
+            sehirler.Nufus = 1000000;
+            sehirler.Ulke = "Azerbeycan";
+            sehirler.Renk1 = "Kırmızı";
+            sehirler.Renk2 = "Turkuaz";
+            sehirler.Renk3 = "Yeşil";
+
+            ViewBag.z1 = sehirler.SehirId;
+            ViewBag.z2 = sehirler.Ulke;
+            ViewBag.z3 = sehirler.SehirAd;
+            ViewBag.z4 = sehirler.Nufus;
+            ViewBag.z5 = sehirler.Renk1;
+            ViewBag.z6 = sehirler.Renk2;
+            ViewBag.z7 = sehirler.Renk3;
+
+
+
+
+            return View();
+        }
     }
 }
---
+++ Proje_OOP/Örnekler/Sehirler.cs
@@ -0,0 +1,10 @@
+﻿namespace Proje_OOP.Örnekler
+{
+    public class Sehirler : Bayrak // Şehirler sınıfı bayrak sınıfının bütün niteliklerini üsütne aldı
+    {
+        public int SehirId { get; set; }
+        public string SehirAd { get; set; } = string.Empty;
+        public int Nufus { get; set; }
+        public string Ulke { get; set; } = string.Empty;
+    }
+}
