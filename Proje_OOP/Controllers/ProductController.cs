de9d5730113982e372c86a5088860aa7c9c02482
bugrabalim11 <bugrablm11@gmail.com>
2026-04-07 13:47:14 +0300

    Ürünler CRUD işlemleri spagetti kod şeklinde yazıldı.

---
+++ Proje_OOP/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OOP.Entity;
+using Proje_OOP.ProjeContext;
+
+namespace Proje_OOP.Controllers
+{
+    public class ProductController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var values = context.Products.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = context.Products.Where(x => x.ProductId == id).FirstOrDefault();
+            context.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = context.Products.Where(x => x.ProductId == id).FirstOrDefault();
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product p)
+        {
+            var value = context.Products.Where(x => x.ProductId == p.ProductId).FirstOrDefault();
+            value.ProductName = p.ProductName;
+            value.ProductPrice = p.ProductPrice;
+            value.ProductStock = p.ProductStock;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
